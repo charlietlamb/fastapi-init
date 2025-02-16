609f15483bc9ad440e75ae4e7e301f396cf617b8
Charlie Lamb <charlielamb20@icloud.com>
2025-02-16 21:33:57 +0000

    init weaviate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
+from ./weaviate import openai_api_key
 
 app = FastAPI()
 
@@ -17,6 +18,6 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-@app.put("/items/{item_id}")
+@app.put("/items/{item_id")
 def update_item(item_id: int, item: Item):
     return { "item_name": item.name, "item_id": item_id }
---
+++ weaviate_client.py
@@ -0,0 +1,18 @@
+import weaviate
+from weaviate.classes.init import Auth
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Best practice: store your credentials in environment variables
+wcd_url = os.environ["WCD_DEMO_URL"]
+wcd_api_key = os.environ["WCD_DEMO_RO_KEY"]
+openai_api_key = os.environ["OPENAI_APIKEY"]
+
+#client = weaviate.connect_to_weaviate_cloud(
+#    cluster_url=wcd_url,
+#    auth_credentials=Auth.api_key(wcd_api_key),
+#    headers={"X-OpenAI-Api-key": openai_api_key},
+#)
